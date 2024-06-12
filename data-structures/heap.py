f70831c71529581061caa1eeda1b24aae12f65cf
Eswar Krishna Maganti <eswarmaganti@Eswars-MacBook-Air.local>
2024-06-12 07:35:54 +0530

    Implemented LinkedList Datastructure

--- data-structures/heap.py
+++ data-structures/heap.py
@@ -1,135 +1,196 @@
-# Implementing Heap Data structure using python
+from typing import List, Optional
+import sys
+import traceback
 
 
-from typing import List,Optional
+# TODO: Have to write MaxHeap Class and comments in code
+
+# function to get the parent index
+def get_parent_index(child_index: int) -> int:
+    return (child_index - 1) // 2
 
 
-class Heap:
-
-    def __init__(self,nodes:List[int]):
-        self.nodes = []
-
-        # creating the heap
-        for node in nodes:
-            self.add_node(node)
-
-    # method which returns the left child node index of parent node in heap
-    def __get_left_child_index(self,parent_index:int) -> int:
-        return (2 * parent_index) +1
-
-    # method which returns the right child node index of parent node in heap
-    def __get_right_child_index(self,parent_index:int)->int:
-        return (2 * parent_index) + 2
-
-    # method which returns the parent node index of left/right child in heap
-    def __get_parent_index(self,child_index:int)->int:
-        return (child_index - 1) // 2
-
-    # method to validate whether the parent node have left child or not
-    def __has_left_child(self,parent_index:int)->bool:
-        return self.__get_left_child_index(parent_index) < len(self.nodes)
-
-    # method to validate whether the parent node have right child or not
-    def __has_right_child(self,parent_index:int)->bool:
-        return self.__get_right_child_index(parent_index) < len(self.nodes)
-
-    # method to validate whether the child node have parent or not
-    def __has_parent(self,child_index:int)->bool:
-        return self.__get_parent_index(child_index) >= 0
-
-    def __left_child(self,parent_index:int)->int:
-
-        return self.nodes[self.__get_left_child_index(parent_index)] if self.__has_left_child(parent_index)  else None
-
-    def __right_child(self,parent_index:int)->int:
-        return self.nodes[self.__get_right_child_index(parent_index)] if self.__has_right_child(parent_index) else None
-
-    def __parent(self,child_index:int) -> int:
-        return self.nodes[self.__get_parent_index(child_index)] if self.__has_parent(child_index) else None
-
-    def swap(self,index_one:int,index_two:int)->int:
-        self.nodes[index_one],self.nodes[index_two] = self.nodes[index_two],self.nodes[index_one]
+# function to get the left child index
+def get_left_child_index(parent_index):
+    return (2 * parent_index) + 1
 
 
-    # method to rearrange the nodes after new element is inserted into heap
-    def __heapify_up(self,child_index:Optional[int]=None):
-        if not child_index:
-            # if child_index is not specified start with end of the tree
-            child_index = len(self.nodes)-1
-
-        parent_index = self.__get_parent_index(child_index)
-
-        # compare the parent and child nodes and perform recursive call till root node
-        if self.__has_parent(child_index) and self.nodes[child_index] < self.nodes[parent_index]:
-            self.swap(child_index,parent_index) # swap the child and parent node positions
-
-            # recursively call the __heapify_up method to rearrange until its accepts the min_heap condition
-            self.__heapify_up(parent_index)
-
-    # method to add a new node to heap
-    def add_node(self,node):
-        # appending the new element at end of heap
-        self.nodes.append(node)
-        # re-arranging the heap nodes
-        self.__heapify_up()
-        # print(self.nodes)
+# function to get the right child index
+def get_right_child_index(parent_index):
+    return (2 * parent_index) + 2
 
 
-    def __heapify_down(self,parent_index:int = 0):
-        if parent_index > len(self.nodes) or not self.__has_left_child(parent_index):
+def has_parent_node(child_index):
+    return get_parent_index(child_index) >= 0
+
+
+# class definition of minheap
+class MinHeap:
+    def __init__(self) -> None:
+        self.nodes: List[int] = []
+
+    # method to swap the values of two nodes
+    def swap_nodes(self, first_index: int, second_index: int) -> None:
+        self.nodes[first_index], self.nodes[second_index] = self.nodes[second_index], self.nodes[first_index]
+
+    def get_min_child_index(self, parent_index: int) -> int:
+        min_child_index = get_left_child_index(parent_index)
+
+        if self.has_right_child(parent_index) and self.nodes[get_right_child_index(parent_index)] < self.nodes[
+            min_child_index]:
+            return get_right_child_index(parent_index)
+        return min_child_index
+
+    def has_left_child(self, parent_index: int) -> bool:
+        return get_left_child_index(parent_index) < len(self.nodes)
+
+    def has_right_child(self, parent_index: int) -> bool:
+        return get_right_child_index(parent_index) < len(self.nodes)
+
+    def heapify_up(self, child_index: Optional[int] = None) -> None:
+        if child_index is None:
+            child_index = len(self.nodes) - 1
+
+        parent_index = get_parent_index(child_index)
+
+        # check whether the parent is grater than the child node value
+        if has_parent_node(child_index) and self.nodes[parent_index] > self.nodes[child_index]:
+            # swap the parent with child
+            self.swap_nodes(parent_index, child_index)
+
+            # recursively traverse the tree to heapify up
+            self.heapify_up(parent_index)
+
+    def heapify_down(self, parent_index: Optional[int] = 0) -> None:
+
+        if parent_index > len(self.nodes) or not self.has_left_child(parent_index):
             return
 
-        # finding the smallest child index
-        smallest_child_index = self.__get_left_child_index(parent_index)
-        # validate whether the right child is present and compare the right and left child values
-        if self.__has_right_child(parent_index) and self.__left_child(parent_index) > self.__right_child(parent_index):
-            smallest_child_index = self.__get_right_child_index(parent_index)
+        min_child_index = self.get_min_child_index(parent_index)
 
-        # validate, swap and call recursive to follow till the end of the tree
-        if self.nodes[parent_index] > self.nodes[smallest_child_index]:
-            # swapping the parent and child nodes
-            self.swap(parent_index,smallest_child_index)
+        if self.nodes[parent_index] > self.nodes[min_child_index]:
+            self.swap_nodes(parent_index, min_child_index)
+            self.heapify_down(min_child_index)
 
-            # re-arranging the nodes
-            self.__heapify_down(smallest_child_index)
+    def insert_node(self, value):
+        self.nodes.append(value)
+        self.heapify_up()
 
-    # method to remove the Root node/ min value from the heap
-    def poll(self)->Optional[int]:
-        # removing the first node
-        removed_node = self.nodes[0]
+    def delete_root_node(self):
+        deleted_node = self.nodes[0]
 
-        # copying the last node to first
         self.nodes[0] = self.nodes[-1]
-
-        # deleting the last node
         del self.nodes[-1]
 
-        #re-arraging the tree
-        self.__heapify_down()
-        return removed_node
+        self.heapify_down()
 
-    # method to check if heap is empty or not
-    def is_empty(self):
-        return not self.nodes
-
-    # method which returns the root node from heap
-    def peek(self)-> int:
-        if not self.is_empty():
-            return  None
-        return self.nodes[0]
-
-    # method to perform heap sort
+        return deleted_node
 
 
-# Method to perform Heap Sort using auxilory space
-def heap_sort():
-    data = [1, 10, 2, 5, 11, 6, 19]
-    obj = Heap(data)
-    sorted_data = []
-    for _ in range(len(data)):
-        sorted_data.append(obj.poll())
-    return sorted_data
+# MaXHeap Class Definition
+class MaxHeap:
 
-# main method
+    def __init__(self):
+        self.nodes: List[int] = []
+
+    # method to check whether the child node has parent node or not
+    def has_parent(self, child_index: int) -> bool:
+        return 0 <= get_parent_index(child_index) < len(self.nodes)
+
+    # method to check whether the parent has left child or not
+    def has_left_child(self, parent_index: int) -> bool:
+        return get_left_child_index(parent_index) < len(self.nodes)
+
+    # method to check whether the parent has right child or not
+    def has_right_child(self, parent_index: int) -> bool:
+        return get_right_child_index(parent_index) < len(self.nodes)
+
+    # helper method to swap the nodes
+    def swap_nodes(self, first_index: int, second_index: int) -> None:
+        self.nodes[first_index], self.nodes[second_index] = self.nodes[second_index], self.nodes[first_index]
+
+    # method to heapify up the tree when new node is inserted
+    def heapify_up(self, child_index: Optional[int] = None) -> None:
+        if child_index is None:
+            child_index = len(self.nodes) - 1
+
+        parent_index = get_parent_index(child_index)
+
+        if self.has_parent(child_index) and self.nodes[child_index] > self.nodes[parent_index]:
+            # swap the parent and child nodes
+            self.swap_nodes(parent_index, child_index)
+            # recursively heapify up till the root node
+            self.heapify_up(parent_index)
+
+    # method to get the max nodes of two children
+    def get_max_child_index(self, parent_index: int) -> int:
+        left_child_index = get_left_child_index(parent_index)
+        if self.has_right_child(parent_index) and self.nodes[left_child_index] < self.nodes[
+            get_right_child_index(parent_index)]:
+            return get_right_child_index(parent_index)
+        return left_child_index
+
+    # method to heapify down the max heap
+    def heapify_down(self, parent_index: int = 0) -> None:
+
+        # return if parent node doesn't have a left child
+        if parent_index >= len(self.nodes) or not self.has_left_child(parent_index):
+            return
+
+        # get the max child of two child nodes
+        max_child_index = self.get_max_child_index(parent_index)
+
+        # heapify the tree
+        if self.nodes[max_child_index] > self.nodes[parent_index]:
+            # swap the child and parent nodes
+            self.swap_nodes(max_child_index, parent_index)
+
+            # recursively heapify down the tree until the last node
+            self.heapify_down(max_child_index)
+
+    # method to insert a new node
+    def insert_node(self, value):
+        self.nodes.append(value)
+        self.heapify_up()
+
+    # method to delete the root of maxheap
+    def delete_root_node(self) -> int:
+        node = self.nodes[0]
+
+        # swap the first node with last node in tree
+        self.nodes[0] = self.nodes[-1]
+        # delete the last node
+        del self.nodes[-1]
+
+        # rearranging the tree
+        self.heapify_down()
+
+        return node
+
+
+# main function
 if __name__ == "__main__":
-    print(heap_sort())
+    try:
+
+        # Min Heap Implementation
+        min_heap = MinHeap()
+        elements = [10,8,5,15,6] # map(int,input("Enter the elements as comma seperated values: ").split(","))
+        for element in elements:
+            min_heap.insert_node(element)
+        print("Array Representation of Min Heap: ",min_heap.nodes)
+        print(min_heap.delete_root_node())
+        print("Array Representation of Min Heap: ",min_heap.nodes)
+
+        # Max Heap implementation
+        max_heap = MaxHeap()
+        elements = [10, 8, 5, 15, 6]  # map(int,input("Enter the elements as comma seperated values: ").split(","))
+        for element in elements:
+            max_heap.insert_node(element)
+        print(f"Array representation of max heap: {max_heap.nodes}")
+        print(f"The Deleted Root Node: {max_heap.delete_root_node()}")
+        print(f"Array representation of max heap: {max_heap.nodes}")
+
+
+    except Exception as e:
+        print(f"*** Error: Something went wrong,  runtime error occurred: {str(e)} ***")
+        traceback.print_exc()
